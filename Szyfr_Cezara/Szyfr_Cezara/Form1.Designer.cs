c7b8519845905d751c1f4aaf3ebbaa0c9d4679f9
Adrian <adino1191@gmail.com>
2020-08-24 15:48:58 +0200

    Add files via upload

---
+++ Szyfr_Cezara/Szyfr_Cezara/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace Szyfr_Cezara
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxSlowo = new System.Windows.Forms.TextBox();
+            this.buttonDeszyfruj = new System.Windows.Forms.Button();
+            this.buttonSzyfr = new System.Windows.Forms.Button();
+            this.textBoxDeszyfr = new System.Windows.Forms.TextBox();
+            this.labelAlfabet = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.labelFile = new System.Windows.Forms.Label();
+            this.buttonOpen = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBoxSlowo
+            // 
+            this.textBoxSlowo.Location = new System.Drawing.Point(15, 48);
+            this.textBoxSlowo.Name = "textBoxSlowo";
+            this.textBoxSlowo.Size = new System.Drawing.Size(144, 20);
+            this.textBoxSlowo.TabIndex = 0;
+            // 
+            // buttonDeszyfruj
+            // 
+            this.buttonDeszyfruj.Location = new System.Drawing.Point(165, 93);
+            this.buttonDeszyfruj.Name = "buttonDeszyfruj";
+            this.buttonDeszyfruj.Size = new System.Drawing.Size(59, 23);
+            this.buttonDeszyfruj.TabIndex = 3;
+            this.buttonDeszyfruj.Text = "Deszyfruj";
+            this.buttonDeszyfruj.UseVisualStyleBackColor = true;
+            this.buttonDeszyfruj.Click += new System.EventHandler(this.buttonDeszyfruj_Click);
+            // 
+            // buttonSzyfr
+            // 
+            this.buttonSzyfr.Location = new System.Drawing.Point(165, 48);
+            this.buttonSzyfr.Name = "buttonSzyfr";
+            this.buttonSzyfr.Size = new System.Drawing.Size(59, 23);
+            this.buttonSzyfr.TabIndex = 4;
+            this.buttonSzyfr.Text = "Szyfruj";
+            this.buttonSzyfr.UseVisualStyleBackColor = true;
+            this.buttonSzyfr.Click += new System.EventHandler(this.buttonSzyfr_Click);
+            // 
+            // textBoxDeszyfr
+            // 
+            this.textBoxDeszyfr.Location = new System.Drawing.Point(15, 93);
+            this.textBoxDeszyfr.Name = "textBoxDeszyfr";
+            this.textBoxDeszyfr.Size = new System.Drawing.Size(144, 20);
+            this.textBoxDeszyfr.TabIndex = 5;
+            // 
+            // labelAlfabet
+            // 
+            this.labelAlfabet.AutoSize = true;
+            this.labelAlfabet.Location = new System.Drawing.Point(12, 9);
+            this.labelAlfabet.Name = "labelAlfabet";
+            this.labelAlfabet.Size = new System.Drawing.Size(46, 13);
+            this.labelAlfabet.TabIndex = 9;
+            this.labelAlfabet.Text = "Alfabet: ";
+            this.labelAlfabet.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Tekst";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 77);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Szyfr";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 123);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(67, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Przesunięcie";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 319);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "Szyfruj";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 203);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(212, 110);
+            this.richTextBox1.TabIndex = 18;
+            this.richTextBox1.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(93, 319);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 19;
+            this.button2.Text = "Deszyfruj";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(15, 146);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(47, 20);
+            this.numericUpDown1.TabIndex = 20;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // labelFile
+            // 
+            this.labelFile.AutoSize = true;
+            this.labelFile.Location = new System.Drawing.Point(90, 177);
+            this.labelFile.Name = "labelFile";
+            this.labelFile.Size = new System.Drawing.Size(35, 13);
+            this.labelFile.TabIndex = 21;
+            this.labelFile.Text = "label7";
+            // 
+            // buttonOpen
+            // 
+            this.buttonOpen.Location = new System.Drawing.Point(12, 172);
+            this.buttonOpen.Name = "buttonOpen";
+            this.buttonOpen.Size = new System.Drawing.Size(75, 23);
+            this.buttonOpen.TabIndex = 22;
+            this.buttonOpen.Text = "Otwórz";
+            this.buttonOpen.UseVisualStyleBackColor = true;
+            this.buttonOpen.Click += new System.EventHandler(this.buttonOpen_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(260, 367);
+            this.Controls.Add(this.buttonOpen);
+            this.Controls.Add(this.labelFile);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labelAlfabet);
+            this.Controls.Add(this.textBoxDeszyfr);
+            this.Controls.Add(this.buttonSzyfr);
+            this.Controls.Add(this.buttonDeszyfruj);
+            this.Controls.Add(this.textBoxSlowo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxSlowo;
+        private System.Windows.Forms.Button buttonDeszyfruj;
+        private System.Windows.Forms.Button buttonSzyfr;
+        private System.Windows.Forms.TextBox textBoxDeszyfr;
+        private System.Windows.Forms.Label labelAlfabet;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label labelFile;
+        private System.Windows.Forms.Button buttonOpen;
+    }
+}
+
---
+++ Szyfr_Cezara/Szyfr_Cezara/Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Text;
+
+namespace Szyfr_Cezara
+{
+    public partial class Form1 : Form
+    {
+        //public string[] salfabet = new string[] { "abcdefghijklmnoprstuwxyz" };
+       // public char[] calfabet = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'r', 's', 't', 'u', 'w', 'x', 'y', 'z' };
+        //public int x = 3;
+        public List<string> lista = new List<string>(new string[] { "a", "ą", "b", "c", "ć", "d", "e", "ę", "f", "g", "h", "i", "j", "k", "l", "ł", "m", "n", "ń", "o", "ó", "p", "r", "s", "ś", "t", "u", "w", "y", "z", "ź", "ż", "A", "Ą", "B", "C", "Ć", "D", "E", "Ę", "F", "G", "H", "I", "J", "K", "L", "Ł", "M", "N", "Ń", "O", "Ó", "P", "R", "S", "Ś", "T", "U", "W", "Y", "Z", "Ź", "Ż", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", " ", "\"", ",", ".", "/", "\\", "{", "}", "[", "]", "(", ")", "-", "_", "+", "-", "*", "!", "?", "\'", "@", "#", "$", "=", "%", "&", "|", "\\n"});
+            
+        
+        public Form1()
+        {
+            InitializeComponent();
+            int l = 0;
+            foreach (string value in lista)
+            {
+                
+                labelAlfabet.Text += lista[l];
+                l++;
+            }
+        }
+        
+
+        private void buttonSzyfr_Click(object sender, EventArgs e)
+        {
+            textBoxDeszyfr.Text = null;
+            //int x = Convert.ToInt32(textBox1.Text);
+            int x = Convert.ToInt32(numericUpDown1.Value);
+            //textBox2.Text = textBox1.Text;
+    
+            string slowo = textBoxSlowo.Text;
+            int dlugosc = slowo.Length;
+            List<string> lista2 = new List<string>();
+            lista2 = lista.ToList();
+            int strIndex = 0;
+            for (int i = 0; i < x; i++)
+            {
+                lista2.Add(lista2[0]);
+                lista2.RemoveAt(0);
+            }
+            int j = lista2.Count;
+            List<string> szyfr = new List<string>();
+            for (int i = 0; i < dlugosc; i++)
+            {
+
+                //strIndex = lista[i].IndexOf(slowo[0]);
+                //
+                strIndex = lista.IndexOf(slowo[i].ToString());
+                szyfr.Add(lista2[strIndex]);
+                textBoxDeszyfr.Text += szyfr[i];
+
+            }
+
+        }
+
+        private void buttonDeszyfruj_Click(object sender, EventArgs e)
+        {
+            textBoxSlowo.Text = null;
+           // int x = Convert.ToInt32(textBox1.Text);
+            int x = Convert.ToInt32(numericUpDown1.Value);
+            string slowo = textBoxDeszyfr.Text;
+            int dlugosc = slowo.Length;
+            List<string> lista2 = new List<string>();
+            lista2 = lista.ToList();
+            int strIndex = 0;
+            for (int i = 0; i < x; i++)
+            {
+                lista2.Add(lista2[0]);
+                lista2.RemoveAt(0);
+            }
+            int j = lista2.Count;
+            List<string> szyfr = new List<string>();
+            for (int i = 0; i < dlugosc; i++)
+            {
+
+                //strIndex = lista[i].IndexOf(slowo[0]);
+                //
+                strIndex = lista2.IndexOf(slowo[i].ToString());
+                szyfr.Add(lista[strIndex]);
+                textBoxSlowo.Text += szyfr[i];
+
+            }
+
+            
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Encoding enc = Encoding.GetEncoding("Windows-1250");
+            string path = labelFile.Text;
+            string slowo = string.Join(" ", File.ReadAllLines(path, enc));
+            //richTextBox1.Text = slowo;
+            richTextBox1.Text = null;
+            int x = Convert.ToInt32(numericUpDown1.Value);
+
+            
+            int dlugosc = slowo.Length;
+            List<string> lista2 = new List<string>();
+            lista2 = lista.ToList();
+            int strIndex = 0;
+            for (int i = 0; i < x; i++)
+            {
+                lista2.Add(lista2[0]);
+                lista2.RemoveAt(0);
+            }
+            int j = lista2.Count;
+            List<string> szyfr = new List<string>();
+            for (int i = 0; i < dlugosc; i++)
+            {
+
+                //strIndex = lista[i].IndexOf(slowo[0]);
+                //
+                strIndex = lista.IndexOf(slowo[i].ToString());
+                szyfr.Add(lista2[strIndex]);
+                richTextBox1.Text += szyfr[i];
+
+            }
+
+            File.WriteAllText("zaszyfrowane.txt", richTextBox1.Text, enc);
+        }
+
+        private void buttonOpen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openfile = new OpenFileDialog();
+            if (openfile.ShowDialog() == DialogResult.OK)
+            {
+                Encoding enc = Encoding.GetEncoding("Windows-1250");
+                labelFile.Text = openfile.FileName;
+                string path = labelFile.Text;
+                //string slowo = File.ReadAllText(path);
+                string slowo = string.Join(" ", File.ReadAllLines(path, enc));
+                richTextBox1.Text = slowo;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Encoding enc = Encoding.GetEncoding("Windows-1250");
+            string path = labelFile.Text;
+            string slowo = string.Join(" ", File.ReadAllLines(path, enc));
+            //richTextBox1.Text = slowo;
+            richTextBox1.Text = null;
+
+            int x = Convert.ToInt32(numericUpDown1.Value);
+            int dlugosc = slowo.Length;
+            List<string> lista2 = new List<string>();
+            lista2 = lista.ToList();
+            int strIndex = 0;
+            for (int i = 0; i < x; i++)
+            {
+                lista2.Add(lista2[0]);
+                lista2.RemoveAt(0);
+            }
+            int j = lista2.Count;
+            List<string> szyfr = new List<string>();
+            for (int i = 0; i < dlugosc; i++)
+            {
+
+                //strIndex = lista[i].IndexOf(slowo[0]);
+                //
+                strIndex = lista2.IndexOf(slowo[i].ToString());
+                szyfr.Add(lista[strIndex]);
+                richTextBox1.Text += szyfr[i];
+
+            }
+
+            File.WriteAllText("odszyfrowane.txt", richTextBox1.Text, enc);
+        }
+    }
+}
